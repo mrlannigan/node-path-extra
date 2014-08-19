f210cfd0a9acad37aa73d6afc1a488a5c7f62062
Julian Lannigan <julian.lannigan@gmail.com>
2014-08-19 01:48:17 -0400

    code formatting of test file

--- test/path.test.js
+++ test/path.test.js
@@ -1,32 +1,32 @@
-var path = require('../lib/path')
-  , fs = require('fs-extra')
-  , assert = require('assert')
+var path = require('../lib/path'),
+    fs = require('fs-extra'),
+    assert = require('assert');
 
+describe('path', function () {
+    describe('+ tempdir()', function () {
+        it('should return a temporary directory', function () {
+            var tmpDir = path.tempdir(),
+                testFile = 'TEST-path-ext-#{Date.now()}',
+                testString = 'SOME STRING',
+                retString;
 
-describe('path', function(){
-     describe('+ tempdir()', function() {
-        it('should return a temporary directory', function() {
-            var tmpDir = path.tempdir();
             assert(tmpDir);
-              
-            var testFile = "TEST-path-ext-#{Date.now()}";
+
             testFile = path.join(tmpDir, testFile);
 
-            var testString = 'SOME STRING';
-
             fs.writeFileSync(testFile, testString);
-            var retString = fs.readFileSync(testFile).toString();
+            retString = fs.readFileSync(testFile).toString();
 
             assert(retString === testString);
         });
-     });
+    });
 
-    describe('+ homedir()', function() {
-        it('should return the users homde directory', function() {
+    describe('+ homedir()', function () {
+        it('should return the users home directory', function () {
             var homeDir = path.homedir();
             assert(homeDir);
         });
-     });
+    });
 });
  
     
